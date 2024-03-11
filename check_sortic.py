5507acf107e3f9553e34653fca1c0d1f9f1f8de7
yasheks <143996832+yasheks@users.noreply.github.com>
2024-03-11 11:11:16 +0300

    Update check_sortic.py

--- check_sortic.py
+++ check_sortic.py
@@ -1,9 +1,18 @@
 from sortic import *
+nums = []
+#comm = []
 sort = []
 num = 0
+commands = ""
 while(num != '!'):
     num = input()
     if num != '!':
         int(num)
-        sort.append(num)
-print(sort)
+        nums.append(num)
+sort = nums.sort()
+while(commands != '*'):
+    commands = str(input())
+    if commands != '*':
+        if commands == sa() or commands == sb() or commands == ra() or commands == rb()
+        nums.commands()
+        
