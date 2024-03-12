1094e182575e52f70a0cbb800847d3f18d1c7ffa
yasheks <143996832+yasheks@users.noreply.github.com>
2024-03-12 19:51:27 +0300

    Update check_sortic.py

--- check_sortic.py
+++ check_sortic.py
@@ -1,8 +1,9 @@
 from sortic import *
 
-nums = []
-comm = []
-sort = []
+nums = [] #добавленные числа(a)
+empty = [] #типа b
+result = [] #результат засчет комманд
+sort = [] #отсортированный список
 num = 0
 commands = ""
 while (num != '!'):
@@ -14,9 +15,30 @@ sort = nums.sort()
 while (commands != '*'):
     commands = str(input())
     if commands != '*':
-        comm = nums.commands()
-print(comm)
-if sort == comm:
+        if commands == "sa":
+            sa(nums)
+        elif commands == "sb":
+            sb(empty)
+        elif commands == "ss":
+            ss(nums, empty)
+        elif commands == "pa":
+            pa(nums, empty)
+        elif commands == "pb":
+            pb(nums, empty)
+        elif commands == "ra":
+            ra(nums)
+        elif commands == "rb":
+            rb(empty)
+        elif commands == "rr":
+            rr(nums, empty)
+        elif commands == "rra":
+            rra(nums)
+        elif commands == "rrb":
+            rrb(empty)
+        elif commands == "rrr":
+            rrr(nums, empty)
+print(nums)
+if sort == nums:
     print("OK")
 else:
     print("KO")
