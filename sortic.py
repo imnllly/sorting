6bb051cb76ba0a418b75a85cd99e29325b1189ef
yasheks <143996832+yasheks@users.noreply.github.com>
2024-03-13 13:05:39 +0300

    Update sortic.py

--- sortic.py
+++ sortic.py
@@ -1,1 +1,20 @@
+from func import *
+num = 0
+nums=[]
+while (num != '!'):
+    num = input()
+    if num != '!':
+        nums.append(int(num))
+while (sorted(nums) != nums):
+    if nums[1] > nums[2]:
+        nums = sa(nums)
+        print("sa")
+    elif nums[1] <= nums[2]:
+        nums = ra(nums)
+        print("ra")
+    elif reversed(nums) == sorted(nums):
+        nums = ra(nums)
+        print("ra")
+    print()
+
 
