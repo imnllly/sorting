d1bbd74f681ae7440387a0501f361d2e5bdfaef9
yasheks <143996832+yasheks@users.noreply.github.com>
2024-03-17 00:49:59 +0300

    Update sortic.py

--- sortic.py
+++ sortic.py
@@ -1,20 +1,20 @@
 from func import *
-num = 0
-nums=[]
-while (num != '!'):
-    num = input()
+
+
+def sort(a):
+    n = len(a)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if a[j] > a[j + 1]:
+                ss(a, [])
+                print("ss")
+            ra(a)
+            print("ra")
+
+a = []
+num = None
+while num != '!':
+    num = input("Введите число или '!' для окончания ввода: ")
     if num != '!':
-        nums.append(int(num))
-while (sorted(nums) != nums):
-    if nums[1] > nums[2]:
-        nums = sa(nums)
-        print("sa")
-    elif nums[1] <= nums[2]:
-        nums = ra(nums)
-        print("ra")
-    elif reversed(nums) == sorted(nums):
-        nums = ra(nums)
-        print("ra")
-    print()
-
-
+        a.append(int(num))
+sort(a)
