c56ce504445aa84a98928310de434952b03a45c2
yasheks <143996832+yasheks@users.noreply.github.com>
2024-03-18 13:02:20 +0300

    Update func.py

--- func.py
+++ func.py
@@ -3,45 +3,65 @@ def sa(a): # работает
         p3 = a[1]
         a[1] = a[0]
         a[0] = p3
-        return a
+    print(a)
+    return a
 
 def sb(b): # работает
     if len(b) > 1:
         p3 = b[1]
         b[1] = b[0]
         b[0] = p3
-        return b
+    print(b)
+    return b
 
 def ss(a, b): # работает
     a = sa(a)
     b = sb(b)
+    print(a, b)
     return a, b
 
 def pa(a, b): # работает
     if len(b) > 0:
         a = a[::-1]
         a.append(b[0])
+        b = b[1::]
         a = a[::-1]
-        return a
+    print(a, b)
+    return a, b
 
 def pb(a, b): # работает
     if len(a) > 0:
         b = b[::-1]
         b.append(a[0])
+        a = a[1::]
         b = b[::-1]
-        return b
+    print(a, b)
+    return a, b
 
 def ra(a): # работает
-    a = a[::-1]
+    if len(a) > 0:
+        c = []
+        for i in range(1, len(a)):
+            c.append(a[i])
+        c.append(a[0])
+        a = c
+    print(a)
     return a
 
 def rb(b): # работает
-    b = b[::-1]
+    if len(b) > 0:
+        c = []
+        for i in range(1, len(b)):
+            c.append(b[i])
+        c.append(b[0])
+        b = c
+    print(b)
     return b
 
 def rr(a, b): # работает
     a = ra(a)
     b = rb(b)
+    print(a, b)
     return a, b
 
 def rra(a): # работает
@@ -51,7 +71,8 @@ def rra(a): # работает
         for i in range(len(a) - 1):
             c.append(a[i])
         a = c
-        return a
+    print(a)
+    return a
 
 def rrb(b): # работает
     if len(b) > 0:
@@ -60,11 +81,13 @@ def rrb(b): # работает
         for i in range(len(b) - 1):
             c.append(b[i])
         b = c
-        return b
+    print(b)
+    return b
 
 
 def rrr(a, b): # работает
     a = rra(a)
     b = rrb(b)
+    print(a, b)
     return a, b
 
